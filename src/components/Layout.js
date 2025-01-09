99b43e01cd47eb05ff89bc3aa255d8c047b36ea3
Zitu Hoq <zhzhitu121@gmail.com>
2025-01-09 20:42:08 +0600

    Add Google Analytics to All Pages

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,9 +1,14 @@
+import { GoogleTagManager } from '@next/third-parties/google'
 import React from 'react'
 
 export default function Layout({children, className=""}) {
   return (
-    <div className={`w-full h-full z-0 bg-light p-32 dark:bg-dark xl:p-24 lg:p-16 md:p-12 sm:p-8 ${className}`}>
+    <>
+      <div className={`w-full h-full z-0 bg-light p-32 dark:bg-dark xl:p-24 lg:p-16 md:p-12 sm:p-8 ${className}`}>
         {children}
     </div>
+    <GoogleTagManager gtmId="G-XVQ6SQX3VQ" />
+    </>
+    
   )
 }
